eafe64fa168d3b56ce3d14391c4268262c0d7f44
SharmaSachin27 <sharmashachin98@gmail.com>
2022-06-04 19:49:50 +0530

    more changes of company pages.

---
+++ resources/views/viewCompany.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.master')
+@section('title', 'Manage Company')
+@section('main-content')
+<main>
+    <div class="container-fluid">
+        <h1 class="mt-4">view Company</h1>
+        <div class="card mb-4">
+            <div class="card-body">
+                
+            </div>
+        </div>
+    </div>
+</main>
+@endsection
